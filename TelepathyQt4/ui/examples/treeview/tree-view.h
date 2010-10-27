8774f025727865b17598b064588cc801d56882c8
Paolo Capriotti <paolo.capriotti@collabora.co.uk>
2010-10-27 16:07:44 +0100

    Add treeview example.

---
+++ TelepathyQt4/ui/examples/treeview/tree-view.h
@@ -0,0 +1,80 @@
+/*
+ * This file is part of TelepathyQt4
+ *
+ * Copyright (C) 2008-2010 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _TelepathyQt4_tree_view_h_HEADER_GUARD_
+#define _TelepathyQt4_tree_view_h_HEADER_GUARD_
+
+#include <QTreeView>
+#include <QWidget>
+
+#include <TelepathyQt4/Types>
+#include <TelepathyQt4/ui/AccountModel>
+
+class TelepathyInitializer;
+
+class TreeView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    
+    TreeView(QWidget *parent = 0);
+
+private Q_SLOTS:
+
+    void onInitializationFinished(TelepathyInitializer *initializer);
+
+private:
+
+    QTreeView *mView;
+};
+
+class TelepathyInitializer : public QObject
+{
+    Q_OBJECT
+
+public:
+    
+    TelepathyInitializer(const Tp::AccountManagerPtr &am);
+
+    void run();
+    Tp::AccountModel *accountModel() const;
+
+Q_SIGNALS:
+    
+    void finished(TelepathyInitializer *initializer);
+
+private Q_SLOTS:
+
+    void onAMReady(Tp::PendingOperation *);
+    void onAccountReady(Tp::PendingOperation *);
+    void onConnectionReady(Tp::PendingOperation *);
+
+private:
+    
+    void initializeConnections();
+
+    Tp::AccountManagerPtr mAM;
+    Tp::AccountModel *mAccountModel;
+    int numConnections;
+};
+
+#endif // _TelepathyQt4_tree_view_h_HEADER_GUARD_
+
