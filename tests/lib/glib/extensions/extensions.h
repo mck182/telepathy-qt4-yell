f253eadac68bfe73165984e94caa9c841c216107
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-01-07 18:29:31 -0200

    Added call cm example from tp-glib with addition of Call.Content.Remove support.

---
+++ tests/lib/glib/extensions/extensions.h
@@ -0,0 +1,18 @@
+#ifndef _EXTENSIONS_H_
+#define _EXTENSIONS_H_
+
+#include <glib-object.h>
+#include <telepathy-glib/channel.h>
+
+#include "tests/lib/glib/extensions/_gen/enums.h"
+#include "tests/lib/glib/extensions/_gen/svc-channel.h"
+#include "tests/lib/glib/extensions/_gen/svc-misc.h"
+
+G_BEGIN_DECLS
+
+#include "tests/lib/glib/extensions/_gen/gtypes.h"
+#include "tests/lib/glib/extensions/_gen/interfaces.h"
+
+G_END_DECLS
+
+#endif
