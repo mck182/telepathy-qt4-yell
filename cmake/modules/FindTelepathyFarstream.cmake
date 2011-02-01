18021c206f7d681cde2aca61478178063c5f82b8
Mateu Batle <mateu.batle@collabora.co.uk>
2011-02-01 11:00:33 -0500

    Changed to find telepathy-farstream library

--- cmake/modules/FindTelepathyFarstream.cmake
+++ cmake/modules/FindTelepathyFarstream.cmake
@@ -1,49 +1,50 @@
-# - Try to find Telepathy-Farsight
+# - Try to find Telepathy-Farstream
 # Once done this will define
 #
-#  TELEPATHY_FARSIGHT_FOUND - system has TelepathyFarsight
-#  TELEPATHY_FARSIGHT_INCLUDE_DIR - the TelepathyFarsight include directory
-#  TELEPATHY_FARSIGHT_LIBRARIES - the libraries needed to use TelepathyFarsight
-#  TELEPATHY_FARSIGHT_DEFINITIONS - Compiler switches required for using TelepathyFarsight
+#  TELEPATHY_FARSTREAM_FOUND - system has TelepathyFarstream
+#  TELEPATHY_FARSTREAM_INCLUDE_DIR - the TelepathyFarstream include directory
+#  TELEPATHY_FARSTREAM_LIBRARIES - the libraries needed to use TelepathyFarstream
+#  TELEPATHY_FARSTREAM_DEFINITIONS - Compiler switches required for using TelepathyFarstream
 
 # Copyright (c) 2010, Dario Freddi <dario.freddi@collabora.co.uk>
+# Copyright (c) 2011, Mateu Batle <mateu.batle@collabora.co.uk>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 
-if (TELEPATHY_FARSIGHT_INCLUDE_DIR AND TELEPATHY_FARSIGHT_LIBRARIES)
+if (TELEPATHY_FARSTREAM_INCLUDE_DIR AND TELEPATHY_FARSTREAM_LIBRARIES)
    # in cache already
-   set(TelepathyFarsight_FIND_QUIETLY TRUE)
-else (TELEPATHY_FARSIGHT_INCLUDE_DIR AND TELEPATHY_FARSIGHT_LIBRARIES)
-   set(TelepathyFarsight_FIND_QUIETLY FALSE)
-endif (TELEPATHY_FARSIGHT_INCLUDE_DIR AND TELEPATHY_FARSIGHT_LIBRARIES)
+   set(TelepathyFarstream_FIND_QUIETLY TRUE)
+else (TELEPATHY_FARSTREAM_INCLUDE_DIR AND TELEPATHY_FARSTREAM_LIBRARIES)
+   set(TelepathyFarstream_FIND_QUIETLY FALSE)
+endif (TELEPATHY_FARSTREAM_INCLUDE_DIR AND TELEPATHY_FARSTREAM_LIBRARIES)
 
 if (NOT WIN32)
     # use pkg-config to get the directories and then use these values
     # in the find_path() and find_library() calls
     find_package(PkgConfig)
-    if (TELEPATHY_FARSIGHT_MIN_VERSION)
-        PKG_CHECK_MODULES(PC_TELEPATHY_FARSIGHT telepathy-farsight>=${TELEPATHY_FARSIGHT_MIN_VERSION})
-    else (TELEPATHY_FARSIGHT_MIN_VERSION)
-        PKG_CHECK_MODULES(PC_TELEPATHY_FARSIGHT telepathy-farsight)
-    endif (TELEPATHY_FARSIGHT_MIN_VERSION)
-    set(TELEPATHY_FARSIGHT_DEFINITIONS ${PC_TELEPATHY_FARSIGHT_CFLAGS_OTHER})
+    if (TELEPATHY_FARSTREAM_MIN_VERSION)
+        PKG_CHECK_MODULES(PC_TELEPATHY_FARSTREAM telepathy-farstream>=${TELEPATHY_FARSTREAM_MIN_VERSION})
+    else (TELEPATHY_FARSTREAM_MIN_VERSION)
+        PKG_CHECK_MODULES(PC_TELEPATHY_FARSTREAM telepathy-farstream)
+    endif (TELEPATHY_FARSTREAM_MIN_VERSION)
+    set(TELEPATHY_FARSTREAM_DEFINITIONS ${PC_TELEPATHY_FARSTREAM_CFLAGS_OTHER})
 endif (NOT WIN32)
 
-find_path(TELEPATHY_FARSIGHT_INCLUDE_DIR telepathy-farsight/channel.h
+find_path(TELEPATHY_FARSTREAM_INCLUDE_DIR telepathy-farstream/channel.h
    PATHS
-   ${PC_TELEPATHY_FARSIGHT_INCLUDEDIR}
-   ${PC_TELEPATHY_FARSIGHT_INCLUDE_DIRS}
+   ${PC_TELEPATHY_FARSTREAM_INCLUDEDIR}
+   ${PC_TELEPATHY_FARSTREAM_INCLUDE_DIRS}
    PATH_SUFFIXES telepathy-1.0
    )
 
-find_library(TELEPATHY_FARSIGHT_LIBRARIES NAMES telepathy-farsight
+find_library(TELEPATHY_FARSTREAM_LIBRARIES NAMES telepathy-farstream
    PATHS
-   ${PC_TELEPATHY_FARSIGHT_LIBDIR}
-   ${PC_TELEPATHY_FARSIGHT_LIBRARY_DIRS}
+   ${PC_TELEPATHY_FARSTREAM_LIBDIR}
+   ${PC_TELEPATHY_FARSTREAM_LIBRARY_DIRS}
    )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(TelepathyFarsight DEFAULT_MSG TELEPATHY_FARSIGHT_LIBRARIES
-                                                                TELEPATHY_FARSIGHT_INCLUDE_DIR)
+find_package_handle_standard_args(TelepathyFarstream DEFAULT_MSG TELEPATHY_FARSTREAM_LIBRARIES
+                                                                TELEPATHY_FARSTREAM_INCLUDE_DIR)
 
-mark_as_advanced(TELEPATHY_FARSIGHT_INCLUDE_DIR TELEPATHY_FARSIGHT_LIBRARIES)
+mark_as_advanced(TELEPATHY_FARSTREAM_INCLUDE_DIR TELEPATHY_FARSTREAM_LIBRARIES)
